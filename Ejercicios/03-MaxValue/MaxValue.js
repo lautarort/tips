0bfa7ad7ca7b2c82fff89492a3473e86f3b763b9
unknown <lautarorangilt@gmail.com>
2021-09-18 12:12:20 -0300

    maxValue

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,21 @@
-function MaxValue(shares) {
+function MaxValue(stocks) {
   // Your code here:
+    // guardamos la primer diferencia
+    let max = stocks[1] - stocks[0];
+    // guardamos el precio mínimo hasta el momento
+    let minPrice = stocks[0];
+    // iteramos sobre las stocks
+    for (let i = 1; i < stocks.length; i += 1) {
+        // sacamos la ganacia potencial
+        const potencial = stocks[i] - minPrice;
+        // Nos quedamos con el mayor valor    
+        max = Math.max(max, potencial);
+        // nos quedamos con el precio mínimo    
+        minPrice = Math.min(minPrice, stocks[i]);
+    }
+    // devolvemos el máximo
+    return max;
+};
 
-}
 
 module.exports = MaxValue;
